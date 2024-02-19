2e13e717646fb4cb7968967b0511b203ce38f07b
Reebal Sami <reebal.sami@gmail.com>
2024-02-19 12:12:42 +0100

    added Controller test getById

--- src/test/java/net/ugurkartal/asterixapi/controllers/AsterixControllerTest.java
+++ src/test/java/net/ugurkartal/asterixapi/controllers/AsterixControllerTest.java
@@ -1,10 +1,12 @@
 package net.ugurkartal.asterixapi.controllers;
 
+import net.ugurkartal.asterixapi.models.Character;
 import net.ugurkartal.asterixapi.repositories.AsterixRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -35,7 +37,25 @@ class AsterixControllerTest {
     }
 
     @Test
-    void getCharacterById() {
+    @DirtiesContext
+    void getCharacterById_shouldReturnAstrix_WhenCalledWithValidId() throws Exception {
+        //GIVEN
+        Character character = new Character("1", "Astrix", 4, "King");
+        asterixRepository.save(character);
+
+        //WHEN & THEN
+        mvc.perform(MockMvcRequestBuilders.get("/asterix/characters/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                {
+                                    "id": "1",
+                                    "name": "Astrix",
+                                    "age": 4,
+                                    "role": "King"
+                                }
+                                """
+                ));
     }
 
     @Test
